b5c3c1c1253917610be76da76a61acca4cdf8183
KelvyHsu <Kelvin.Hsu@nicta.com.au>
2015-08-28 00:23:19 +1000

    Preparing to visualise snapshots

---
+++ informative-seafloor-exploration/scott_reef_snapshots.py
@@ -0,0 +1,16 @@
+import numpy as np
+import shelve
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import logging
+import time
+import matplotlib.ticker as ticker
+
+def main():
+
+	i_trials = np.arange(25, 201, 25)
+	print(i_trials)
+
+if __name__ == "__main__":
+    main()
